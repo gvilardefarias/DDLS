7e90fd42ec72674d52798c9c433f3aa08c7fa7cc
Gustavo Vilar <gvilardefarias@ymail.com>
2017-07-20 17:54:27 -0300

    Conversão do Bit Counter para 8 bits

--- rtl/Bit Counter/bitCounter.sv
+++ rtl/Bit Counter/bitCounter.sv
@@ -1,35 +1,35 @@
 module bitCounter(DDLS_if.Basic iter);
-	logic[31:0] word;
-	logic[5:0] counterBits, posCounter;
+	logic[7:0] word;
+	logic[3:0] counterBits, posCounter;
 
 	always_ff @(posedge iter.clk)
 		if(iter.rst) begin
-			word <= 32'd0;
+			word <= 8'd0;
 			iter.Ready <= 1'b1;
-			posCounter <= 6'd0;
-			counterBits <= 6'd0;
-			iter.DataOut <= 32'd0;	
+			posCounter <= 4'd0;
+			counterBits <= 4'd0;
+			iter.DataOut <= 8'd0;	
 		end
 		else begin
 			if(iter.Valid && iter.Ready) begin
-				posCounter <= 6'd0;
+				posCounter <= 4'd0;
 				iter.Ready <= 1'b0;
-				counterBits <= 6'd0;
+				counterBits <= 4'd0;
 				word <= iter.DataIn;
 			end
 			if(~iter.Ready) begin
-				posCounter <= posCounter + 6'd1;
+				posCounter <= posCounter + 4'd1;
 				
-				if(posCounter<6'd31)
-					if(word[31])
+				if(posCounter<4'd7)
+					if(word[7])
 						counterBits <= counterBits + word[posCounter];
 					else begin
 						if(~word[posCounter])
-							counterBits <= counterBits + 6'd1;
+							counterBits <= counterBits + 4'd1;
 					end
 				else begin
 					iter.Ready <= 1'b1;
-					posCounter <= 6'd0;
+					posCounter <= 4'd0;
 					iter.DataOut <= counterBits;
 				end
 			end			
--- rtl/DDLS_if.sv
+++ rtl/DDLS_if.sv
@@ -1,6 +1,6 @@
 interface DDLS_if(input logic clk, input logic rst);
 	logic Valid, Ready;
-	logic[31:0] DataIn, DataOut;
+	logic[7:0] DataIn, DataOut;
 	
 	modport Basic(input clk, rst, Valid, DataIn, output Ready, DataOut);
 	
